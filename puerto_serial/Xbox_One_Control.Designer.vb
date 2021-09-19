cc10802d018671551b0e00ac974462a2b9dc128a
Noel <imnooee1@gmail.com>
2021-09-19 17:27:12 -0500

    first release

---
+++ puerto_serial/Xbox_One_Control.Designer.vb
@@ -0,0 +1,402 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Xbox_One_Control
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Xbox_One_Control))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.d1 = New System.Windows.Forms.TextBox()
+        Me.d2 = New System.Windows.Forms.TextBox()
+        Me.d3 = New System.Windows.Forms.TextBox()
+        Me.d4 = New System.Windows.Forms.TextBox()
+        Me.d8 = New System.Windows.Forms.TextBox()
+        Me.d7 = New System.Windows.Forms.TextBox()
+        Me.d5 = New System.Windows.Forms.TextBox()
+        Me.d9 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Poor Richard", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(30, 36)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Cargar puertos"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(160, 35)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(114, 30)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Poor Richard", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(30, 89)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 44)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Conectar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.BaudRate = 115200
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(300, 35)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(142, 28)
+        Me.TextBox1.TabIndex = 14
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Poor Richard", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button9.Location = New System.Drawing.Point(320, 67)
+        Me.Button9.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(93, 35)
+        Me.Button9.TabIndex = 15
+        Me.Button9.Text = "Enviar"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(537, 68)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(155, 34)
+        Me.TextBox3.TabIndex = 24
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(594, 38)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 24)
+        Me.Label1.TabIndex = 29
+        Me.Label1.Text = "Input"
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 250
+        '
+        'd1
+        '
+        Me.d1.Location = New System.Drawing.Point(52, 199)
+        Me.d1.Margin = New System.Windows.Forms.Padding(2)
+        Me.d1.Name = "d1"
+        Me.d1.Size = New System.Drawing.Size(142, 28)
+        Me.d1.TabIndex = 30
+        '
+        'd2
+        '
+        Me.d2.Location = New System.Drawing.Point(52, 258)
+        Me.d2.Margin = New System.Windows.Forms.Padding(2)
+        Me.d2.Name = "d2"
+        Me.d2.Size = New System.Drawing.Size(142, 28)
+        Me.d2.TabIndex = 31
+        '
+        'd3
+        '
+        Me.d3.Location = New System.Drawing.Point(378, 199)
+        Me.d3.Margin = New System.Windows.Forms.Padding(2)
+        Me.d3.Name = "d3"
+        Me.d3.Size = New System.Drawing.Size(142, 28)
+        Me.d3.TabIndex = 32
+        '
+        'd4
+        '
+        Me.d4.Location = New System.Drawing.Point(218, 258)
+        Me.d4.Margin = New System.Windows.Forms.Padding(2)
+        Me.d4.Name = "d4"
+        Me.d4.Size = New System.Drawing.Size(142, 28)
+        Me.d4.TabIndex = 33
+        '
+        'd8
+        '
+        Me.d8.Location = New System.Drawing.Point(556, 258)
+        Me.d8.Margin = New System.Windows.Forms.Padding(2)
+        Me.d8.Name = "d8"
+        Me.d8.Size = New System.Drawing.Size(142, 28)
+        Me.d8.TabIndex = 37
+        '
+        'd7
+        '
+        Me.d7.Location = New System.Drawing.Point(556, 199)
+        Me.d7.Margin = New System.Windows.Forms.Padding(2)
+        Me.d7.Name = "d7"
+        Me.d7.Size = New System.Drawing.Size(142, 28)
+        Me.d7.TabIndex = 36
+        '
+        'd5
+        '
+        Me.d5.Location = New System.Drawing.Point(218, 199)
+        Me.d5.Margin = New System.Windows.Forms.Padding(2)
+        Me.d5.Name = "d5"
+        Me.d5.Size = New System.Drawing.Size(142, 28)
+        Me.d5.TabIndex = 34
+        '
+        'd9
+        '
+        Me.d9.Location = New System.Drawing.Point(378, 258)
+        Me.d9.Margin = New System.Windows.Forms.Padding(2)
+        Me.d9.Name = "d9"
+        Me.d9.Size = New System.Drawing.Size(142, 28)
+        Me.d9.TabIndex = 38
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(89, 170)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 24)
+        Me.Label2.TabIndex = 39
+        Me.Label2.Text = "AxisLeftX"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(89, 236)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(96, 24)
+        Me.Label3.TabIndex = 40
+        Me.Label3.Text = "AxisLeftY"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(245, 170)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(113, 24)
+        Me.Label4.TabIndex = 41
+        Me.Label4.Text = "AxisRightX"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(245, 236)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(111, 24)
+        Me.Label5.TabIndex = 42
+        Me.Label5.Text = "AxisRightY"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.Color.Red
+        Me.Label6.Location = New System.Drawing.Point(403, 170)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(88, 24)
+        Me.Label6.TabIndex = 43
+        Me.Label6.Text = "Triggers"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.Red
+        Me.Label7.Location = New System.Drawing.Point(418, 236)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(53, 24)
+        Me.Label7.TabIndex = 44
+        Me.Label7.Text = "POV"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.ForeColor = System.Drawing.Color.Red
+        Me.Label8.Location = New System.Drawing.Point(590, 170)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(79, 24)
+        Me.Label8.TabIndex = 45
+        Me.Label8.Text = "Buttons"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.ForeColor = System.Drawing.Color.Red
+        Me.Label9.Location = New System.Drawing.Point(569, 236)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(160, 24)
+        Me.Label9.TabIndex = 46
+        Me.Label9.Text = "Buttons pressed"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(756, 23)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 100)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 47
+        Me.PictureBox1.TabStop = False
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.ForeColor = System.Drawing.Color.Red
+        Me.Label10.Location = New System.Drawing.Point(742, 138)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(159, 24)
+        Me.Label10.TabIndex = 48
+        Me.Label10.Text = "By Call Me Noel"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(745, 194)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(126, 95)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 49
+        Me.PictureBox2.TabStop = False
+        '
+        'Xbox_One_Control
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 22.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(876, 301)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.d9)
+        Me.Controls.Add(Me.d8)
+        Me.Controls.Add(Me.d7)
+        Me.Controls.Add(Me.d5)
+        Me.Controls.Add(Me.d4)
+        Me.Controls.Add(Me.d3)
+        Me.Controls.Add(Me.d2)
+        Me.Controls.Add(Me.d1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Xbox_One_Control"
+        Me.Text = "XboxOneControl"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents d1 As TextBox
+    Friend WithEvents d2 As TextBox
+    Friend WithEvents d3 As TextBox
+    Friend WithEvents d4 As TextBox
+    Friend WithEvents d8 As TextBox
+    Friend WithEvents d7 As TextBox
+    Friend WithEvents d5 As TextBox
+    Friend WithEvents d9 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ puerto_serial/Xbox_One_Control.vb
@@ -0,0 +1,140 @@
+﻿Imports System.IO.Ports
+Imports System.Speech.Recognition
+Imports System.Runtime.InteropServices
+
+Public Class Xbox_One_Control
+
+    Public mensajesalida As String, mensajeentrada As String, x As Integer
+    Declare Function joyGetPosEx Lib "winmm.dll" (ByVal uJoyID As Integer, ByRef pji As JOYINFOEX) As Integer
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure JOYINFOEX
+        Public dwSize As Integer
+        Public dwFlags As Integer
+        Public dwXpos As Integer
+        Public dwYpos As Integer
+        Public dwZpos As Integer
+        Public dwRpos As Integer
+        Public dwUpos As Integer
+        Public dwVpos As Integer
+        Public dwButtons As Integer
+        Public dwButtonNumber As Integer
+        Public dwPOV As Integer
+        Public dwReserved1 As Integer
+        Public dwReserved2 As Integer
+    End Structure
+
+    Dim myjoyEX As JOYINFOEX
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button2.Enabled = False
+
+        myjoyEX.dwSize = 64
+        myjoyEX.dwFlags = &HFF ' All information
+        Timer2.Interval = 10  'Update at 5 hz
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'limpia los datos del combo
+        ComboBox1.Items.Clear()
+        'ínicio un ciclo para cargar los puertos
+        For Each puerto_disponible In My.Computer.Ports.SerialPortNames
+            ComboBox1.Items.Add(puerto_disponible)
+        Next
+        If (ComboBox1.Items.Count > 0) Then
+            ComboBox1.Text = ComboBox1.Items(0)
+            Button2.Enabled = True
+        Else
+            MsgBox("no existe puertos disponibles")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (Button2.Text = "Conectar") Then
+            Button2.Text = "Desconectar"
+            SerialPort1.PortName = ComboBox1.Text
+            SerialPort1.Open()
+            Timer1.Enabled = True
+            Timer2.Enabled = True
+        ElseIf (Button2.Text = "Desconectar") Then
+            Button2.Text = "Conectar"
+            SerialPort1.Close()
+            Timer1.Enabled = False
+            Timer2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+
+
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Dim Buffer As String
+
+        ' Get the joystick information
+        Call joyGetPosEx(0, myjoyEX)
+
+        With myjoyEX
+            d1.Text = .dwXpos.ToString          'Up to six axis supported
+            d2.Text = .dwYpos.ToString
+            d3.Text = .dwZpos.ToString
+            d4.Text = .dwRpos.ToString
+            d5.Text = .dwUpos.ToString
+            d7.Text = .dwButtons.ToString("X")  'Print in Hex, so can see the individual bits associated with the buttons
+            d8.Text = .dwButtonNumber.ToString  'number of buttons pressed at the same time
+            d9.Text = (.dwPOV / 100).ToString     'POV hat (in 1/100ths of degrees, so divided by 100 to give degrees)
+
+
+            Buffer = d1.Text + ";" + d2.Text + ";" + d5.Text + ";" + d4.Text + ";" + d3.Text + ";" + d9.Text + ";" + d7.Text + ";" + d8.Text + ";"
+            SerialPort1.DiscardOutBuffer()
+            SerialPort1.WriteLine(Buffer)
+
+
+        End With
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        '' SerialPort1.WriteLine("128;1223;3234;2321;3245;98;")
+
+        SerialPort1.DiscardOutBuffer()
+        mensajesalida = TextBox1.Text
+        SerialPort1.WriteLine(mensajesalida)
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub d2_TextChanged(sender As Object, e As EventArgs) Handles d2.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        mensajeentrada = SerialPort1.ReadExisting
+        TextBox3.Text = mensajeentrada
+
+        Dim y1 As Integer, y2 As Integer
+        Dim a() As String, s As String
+
+        s = mensajeentrada
+        a = Split(s, ";")
+
+
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+
+    End Sub
+End Class
